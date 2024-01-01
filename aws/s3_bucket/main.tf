7126dd134a1495b8a72c881b4ffab4773ede3e1e
Andre Rocha <andrerocha546@gmail.com>
2024-01-01 14:51:37 -0300

    basic terraform aws s3 bucket

---
+++ aws/s3_bucket/main.tf
@@ -0,0 +1,45 @@
+resource "aws_s3_bucket" "static_site_bucket" {
+    bucket = "static-site-${var.bucket_name}"
+    acl    = "private"
+
+    website {
+        index_document = "index.html"
+        error_document = "error.html"
+    }
+
+    tags = {
+        Bucket = "Bucket Tag"
+        Environment = "Environment Tag"
+    }
+
+}
+
+# alterando configurações de bloqueios de acesso publico
+resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
+    bucket = aws_s3_bucket.static_site_bucket.id
+
+    block_public_acls       = false
+    block_public_policy     = false
+    ignore_public_acls      = false
+    restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
+    bucket = aws_s3_bucket.static_site_bucket.id
+    rule {
+        object_ownership = "BucketOwnerPreferred"
+    }
+}
+
+# anexando politica de acesso publico
+resource "aws_s3_bucket_acl" "static_site_bucket" {
+
+    depends_on = [
+        aws_s3_bucket_public_access_block.static_site_bucket,
+        aws_s3_bucket_ownership_controls.static_site_bucket,
+    ]
+    bucket = aws_s3_bucket.static_site_bucket.id
+
+
+    acl    = "public-read" 
+}
---
+++ aws/s3_bucket/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+    description = "AWS Region"
+    type        = string
+    default     = "us-east-2"
+}
+
+variable "bucket_name" {
+    description = "S3 Bucket Name"
+    type        = string
+    default     = "andrerocha546"
+}
